da35f57db3c29f6864494ae5bce7b5dc6f96f150
Johnathan Chei <j.chei175@gmail.com>
2015-07-28 14:48:04 -0400

    changed port

--- eightBall_server.js
+++ eightBall_server.js
@@ -1,5 +1,5 @@
 var net = require('net');
-var port = 3000;
+var port = 5000;
 var answers = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
 var server = net.createServer(function(client){
 	console.log("Client connected.\r\n")

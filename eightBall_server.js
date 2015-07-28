fbe41d8f3a79dbdd83e66b6bc880c52ef36b9aed
Johnathan Chei <j.chei175@gmail.com>
2015-07-28 15:16:55 -0400

    fixed maybe

--- eightBall_server.js
+++ eightBall_server.js
@@ -1,5 +1,5 @@
 var net = require('net');
-var port = 5000;
+var port = 3000;
 var answers = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
 var server = net.createServer(function(client){
 	console.log("Client connected.\r\n")
@@ -12,12 +12,14 @@ var server = net.createServer(function(client){
 			client.write("Please ask me a 'yes' or 'no' question with a '?'")
 		}
 
-	})
+	});
+
 	client.on("end",function(){
 		console.log("Client disconnected.\r\n");
-	})
+	});
 
-})
-server.listen(function(){
-	console.log("Listening on "+port+".\r\n");
+});
+
+server.listen(port, function(){
+	console.log("Listening on "+port);
 })
